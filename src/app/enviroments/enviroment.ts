0a292329a20e40a7a9af656863e6754c9756b154
Fil88-6912 <f.balda15@yahoo.it>
2024-10-10 23:43:16 +0200

    Add files via upload

---
+++ src/app/enviroments/enviroment.ts
@@ -0,0 +1,5 @@
+export const enviroment = {
+    API_USER_URL: 'http://localhost:5073/api/users',
+    API_LOGIN_URL: 'http://localhost:5073/login',
+    API_REGISTER_URL: 'http://localhost:5073/register'
+}
