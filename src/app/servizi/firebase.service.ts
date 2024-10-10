e716ece335e52dd8dbecc7d5c85508864970d3b4
Fil88-6912 <f.balda15@yahoo.it>
2024-10-10 23:57:59 +0200

    Update firebase.service.ts

--- src/app/servizi/firebase.service.ts
+++ src/app/servizi/firebase.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { AuthService } from '../auth/auth.service'; 
 import { BehaviorSubject, of, Subject, tap } from 'rxjs';
+import { enviroment } from '../enviroments/enviroment';
 
 export interface User {
   nome: string;
@@ -14,7 +15,7 @@ export interface User {
 })
 export class FirebaseService {
   //url = 'https://myappdb-8f7f7-default-rtdb.europe-west1.firebasedatabase.app/persone'
-  url = 'http://localhost:5073/api/users'
+  private URL = enviroment.API_USER_URL;
   userCreated = new Subject<any>();
   userDeleted = new Subject<any>();
   userUpdated = new Subject<any>();
@@ -29,8 +30,8 @@ export class FirebaseService {
   }
 
   insertPersona(body: {}){
-    console.log(`${this.url}.json?auth=${this.authService.user!.token}`, body)
-    return this.http.post(this.url, body)
+    //console.log(`${this.URL}.json?auth=${this.authService.user!.token}`, body)
+    return this.http.post(this.URL, body)
     .pipe(
       tap(res => {
         this.persone = [];
@@ -40,20 +41,20 @@ export class FirebaseService {
   }
   
   getPersone(){
-    console.log(`${this.url}.json?auth=${this.authService.user!.token}`)
+    //console.log(`${this.URL}.json?auth=${this.authService.user!.token}`)
     if(this.persone.length){
       return of(this.persone)
     }
-    return this.http.get(this.url)
+    return this.http.get(this.URL)
     .pipe(
       tap(res => this.persone = res)
     );
   }
 
   deletePersona(id: string){
-    console.log('deletePersona url')
-    console.log(`${this.url}/${id}.json?auth=${this.authService.user!.token}`)
-    return this.http.delete(`${this.url}/${id}`)
+    //console.log('deletePersona url')
+    //console.log(`${this.URL}/${id}.json?auth=${this.authService.user!.token}`)
+    return this.http.delete(`${this.URL}/${id}`)
     .pipe(
       tap(res => {
         this.persone = [];
@@ -63,13 +64,13 @@ export class FirebaseService {
   }
 
   getPersona(id: string){
-    console.log(`${this.url}/${id}.json?auth=${this.authService.user!.token}`)
-    return this.http.get(`${this.url}/${id}`)
+    //console.log(`${this.URL}/${id}.json?auth=${this.authService.user!.token}`)
+    return this.http.get(`${this.URL}/${id}`)
   }
 
   patchPersona(id: string, body: {}){
-    console.log(`${this.url}/${id}.json?auth=${this.authService.user!.token}`)
-    return this.http.patch(`${this.url}/${id}`, body)
+    //console.log(`${this.URL}/${id}.json?auth=${this.authService.user!.token}`)
+    return this.http.patch(`${this.URL}/${id}`, body)
     .pipe(
       tap(res => {
         this.persone = [];
